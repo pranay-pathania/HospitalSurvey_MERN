3b05ab848b80148fd8bba8c08842bfe1c5942f14
pranay-pathania <88281503+pranay-pathania@users.noreply.github.com>
2021-08-28 00:06:27 +0530

    Add files via upload

---
+++ frontend/src/initialStateValues.js
@@ -0,0 +1,44 @@
+export const initialState = {
+    name: '',
+    age: '',
+    phone: '',
+    email: '',
+    conditions: {
+        Asthma: false,
+        Cancer: false,
+        Cardiac_Diseases: false,
+        Diabetes: false,
+        Hypertension: false,
+        Psychiatric_Disorders: false,
+        Epilepsy: false
+    },
+    medication: '',
+    allergy: '',
+    tobacco: '',
+    drugs: '',
+    alcohol: '',
+    gender: ''
+}
+
+export const initialGeneralData = {
+    name: '',
+    age: '',
+    phone: '',
+    email: '',
+    medication: '',
+    allergy: '',
+    tobacco: '',
+    drugs: '',
+    alcohol: '',
+    gender: '',
+}
+
+export const initialCondition = {
+    Asthma: false,
+    Cancer: false,
+    Cardiac_Diseases: false,
+    Diabetes: false,
+    Hypertension: false,
+    Psychiatric_Disorders: false,
+    Epilepsy: false
+}
